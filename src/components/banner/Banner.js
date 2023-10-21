8468360338e54cc57e21feca73880ea447101d09
Вадим <andreeshinvadim13@gmail.com>
2023-10-21 09:43:48 +0300

    Fixed Banner
    And deleted some uneusable images

--- src/components/banner/Banner.js
+++ src/components/banner/Banner.js
@@ -1,10 +1,5 @@
 import "../banner/banner.css"
-import table from "../../images/Table3.png"
-import spagetti from "../../images/Spaghetti3.png"
-import lady1 from "../../images/lady 1.png"
-import lady2 from "../../images/Lady2 1.png"
-import cognac from "../../images/Cognac.png"
-import mojito from "../../images/Mojito.png"
+import ladies from "../../images/BothLadies.svg"
 
 function Banner() {
   return (
@@ -12,14 +7,9 @@ function Banner() {
       <h2>
         <span>Hungry?</span> Find your next meal
       </h2>
-      <button>See all restaurants</button>
+      <button>View all restaurants</button>
       <div className="images">
-        <img src={table} alt="table_img" className="table" />
-        <img src={spagetti} alt="spagetti_img" className="spaghetti" />
-        <img src={lady1} alt="lady1_img" className="lady1" />
-        <img src={lady2} alt="lady2_img" className="lady2" />
-        <img src={cognac} alt="cognac_img" className="cognac" />
-        <img src={mojito} alt="mojito_img" className="mojito" />
+        <img src={ladies} alt="ladies_img" className="ladies" />
       </div>
     </div>
   )
