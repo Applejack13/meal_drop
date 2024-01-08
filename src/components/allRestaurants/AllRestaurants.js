b0c34c9056a0eb0dfc76bf3157972fa1f3009016
Вадим <andreeshinvadim13@gmail.com>
2024-01-08 15:58:00 +0200

    Just create a new component

--- src/components/allRestaurants/AllRestaurants.js
+++ src/components/allRestaurants/AllRestaurants.js
@@ -1,12 +1,13 @@
 import "../allRestaurants/allRestaurants.css"
 import TopHeader from "../topHeader/TopHeader"
 import Footer from "../footer/Footer"
+import ExchangeRates from "../exchangeRates/ExchangeRates"
 
 function AllRestaurants() {
   return (
     <div className="allRestaurants">
       <TopHeader />
-
+      <ExchangeRates />
       <Footer />
     </div>
   )
---
+++ src/components/exchangeRates/ExchangeRates.js
@@ -0,0 +1,9 @@
+import "../exchangeRates/exchangeRates.css"
+
+export default function ExchangeRates() {
+  return (
+    <div className="exchangeRatesWrapper">
+      <h2>Exchange Rates:</h2>
+    </div>
+  )
+}
