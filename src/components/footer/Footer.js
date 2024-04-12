e5edfb48be3dbccf20464c8fcce4791edfba6509
Вадим <andreeshinvadim13@gmail.com>
2024-04-12 13:36:31 +0300

    Added an animation for footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,10 +1,43 @@
+import React, { useEffect, useState } from "react";
 import { Logo, LinkInFooter } from "../../allPages";
-
 import "./../footer/footer.scss";
 
 export function Footer() {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    function reveal() {
+      var reveals = document.querySelectorAll(".reveal");
+
+      for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 100;
+
+        if (elementTop < windowHeight - elementVisible) {
+          reveals[i].classList.add("active");
+        } else {
+          reveals[i].classList.remove("active");
+        }
+      }
+    }
+
+    window.addEventListener("scroll", reveal);
+
+    return () => {
+      window.removeEventListener("scroll", reveal);
+    };
+  }, []);
+
+  useEffect(() => {
+    setIsVisible(true);
+    setTimeout(() => {
+      setIsVisible(false);
+    }, 1000);
+  }, []);
+
   return (
-    <div className='footer'>
+    <div className={`footer ${isVisible ? "reveal active" : "reveal"}`}>
       <div className='footer-container'>
         <div>
           <Logo isSmall={false} />
